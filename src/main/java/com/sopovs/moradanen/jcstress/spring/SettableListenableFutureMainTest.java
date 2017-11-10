befc2f95fde36b22e1fd0f1e02b24a476acd45aa
Ivan Sopov <i.sopov@corp.mail.ru>
2017-11-10 12:21:32 +0300

    another java6 fix

--- src/main/java/com/sopovs/moradanen/jcstress/spring/SettableListenableFutureMainTest.java
+++ src/main/java/com/sopovs/moradanen/jcstress/spring/SettableListenableFutureMainTest.java
@@ -2,96 +2,44 @@ package com.sopovs.moradanen.jcstress.spring;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
-import org.springframework.util.concurrent.FailureCallback;
 import org.springframework.util.concurrent.SettableListenableFuture;
-import org.springframework.util.concurrent.SuccessCallback;
 
 public class SettableListenableFutureMainTest {
 
-	public static void main(String[] args) {
-		BlockingQueue<ResultFutureContainer> queue1 = new LinkedBlockingQueue<ResultFutureContainer>(),
-				queue2 = new LinkedBlockingQueue<ResultFutureContainer>();
-		Thread thread1 = new SuccessThread(queue1), thread2 = new ExceptionThread(queue2);
+	public static void main(String[] args) throws InterruptedException {
+		BlockingQueue<SettableListenableFuture<String>> queue = new LinkedBlockingQueue<SettableListenableFuture<String>>();
+		Thread thread1 = new ProducerThread(queue), thread2 = new ConsumerThread(queue);
 		thread1.start();
 		thread2.start();
-		for (int i = 0; i < 1000; i++) {
-			ResultFutureContainer container = new ResultFutureContainer();
-			queue1.add(container);
-			queue2.add(container);
-			while (!container.exceptionRun || !container.successRun) {
-				Thread.yield();
-			}
-			if (container.result.sucessCallback && !container.result.success) {
-				System.out.println("SuccessCallback without success!");
-			}
-			if (container.result.failCallback && !container.result.exception) {
-				System.out.println("FailCallback without fail!");
-			}
-		}
+		Thread.sleep(TimeUnit.SECONDS.toMillis(10));
 		thread1.interrupt();
 		thread2.interrupt();
+		System.out.println("Done!");
 	}
 
-	public static class Result {
-		public volatile boolean success, sucessCallback, exception, failCallback;
-	}
+	static class ProducerThread extends Thread {
+		private final BlockingQueue<SettableListenableFuture<String>> queue;
 
-	public static class ResultFutureContainer {
-		public final Result result = new Result();
-		public volatile boolean successRun, exceptionRun;
-		public final SettableListenableFuture<String> future = new SettableListenableFuture<String>();
-
-		public ResultFutureContainer() {
-			future.addCallback(
-					new SuccessCallback<String>() {
-
-						@Override
-						public void onSuccess(String unused) {
-							result.sucessCallback = true;
-
-						}
-					},
-					new FailureCallback() {
-
-						@Override
-						public void onFailure(Throwable ex) {
-							result.failCallback = true;
-
-						}
-
-					});
-
-		}
-	}
-
-	static class SuccessThread extends Thread {
-		private final BlockingQueue<ResultFutureContainer> queue;
-
-		public SuccessThread(BlockingQueue<ResultFutureContainer> queue) {
+		public ProducerThread(BlockingQueue<SettableListenableFuture<String>> queue) {
 			this.queue = queue;
 		}
 
 		@Override
 		public void run() {
 			while (!this.isInterrupted()) {
-				try {
-					ResultFutureContainer container = queue.take();
-					container.result.success = container.future.set("foo");
-					container.successRun = true;
-				} catch (InterruptedException e) {
-					this.interrupt();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+				SettableListenableFuture<String> ss = new SettableListenableFuture<String>();
+				ss.set("FooBar!");
+				queue.offer(ss);
 			}
 		}
 	}
 
-	static class ExceptionThread extends Thread {
-		private final BlockingQueue<ResultFutureContainer> queue;
+	static class ConsumerThread extends Thread {
+		private final BlockingQueue<SettableListenableFuture<String>> queue;
 
-		public ExceptionThread(BlockingQueue<ResultFutureContainer> queue) {
+		public ConsumerThread(BlockingQueue<SettableListenableFuture<String>> queue) {
 			this.queue = queue;
 		}
 
@@ -99,9 +47,11 @@ public class SettableListenableFutureMainTest {
 		public void run() {
 			while (!this.isInterrupted()) {
 				try {
-					ResultFutureContainer container = queue.take();
-					container.result.exception = container.future.setException(new Exception());
-					container.exceptionRun = true;
+					SettableListenableFuture<String> future = queue.take();
+					if (null == future.get()) {
+						System.out.println("WAT!");
+						return;
+					}
 				} catch (InterruptedException e) {
 					this.interrupt();
 				} catch (Exception e) {
